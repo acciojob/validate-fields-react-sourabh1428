1f4fb5985370e84762619ddec05c2c224cc161ab
sourabh1428 <sppathak1428@gmail.com>
2023-11-21 16:43:03 +0530

    awsa

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[state,setState]=useState({name:'',password:''});
+  const[err,setErr]=useState(false);
+  function handleChange(event){
+    setState({...state,[event.target.name]:event.target.value});
+  }
+
+  function handleClick(){
+    
+    event.preventDefault();
+    if(state.name==='' || state.password===''){
+      setErr(true);
+    }
+    else{
+      setErr(false);
+    }    console.log(state);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <form onSubmit={handleClick}>
+      <input type="text" name="name" value={state.name} onChange={handleChange}/>
+      <input type="password" name="password" value={state.password} onChange={handleChange} />
+      <button>Submit</button>
+      {err&&(<h1>err</h1>)}
+       </form>
+
+
+
     </div>
   )
 }
